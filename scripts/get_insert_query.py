e90830793b3b5303fba1cf806127d60e1ef949a7
nikita <dayzwaitik123@yandex.ru>
2025-06-08 17:08:42 +0300

    remove unused cript

--- scripts/get_insert_query.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-
-df = pd.read_csv("static/for_query.csv")
-print(*[f"(\'{row['NumCode']}\', \'{row['CharCode']}\', \'{row['Name']}\')" for _, row in df.iterrows()], sep=",\n", end=";")
